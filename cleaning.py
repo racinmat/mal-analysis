864c23e56e39da770b7a7e4e20e53c8794f880b3
Matěj Račinský <racinsky.matej@seznam.cz>
2019-07-28 17:00:28 +0200

    done some cleaning and moving features between notebooks

--- cleaning.py
+++ cleaning.py
@@ -4,8 +4,8 @@ import pandas as pd
 from pandas.io.common import _NA_VALUES
 
 
-def get_users_for_episode_count_ban(animelists: pd.DataFrame) -> pd.Series:
-    users_for_ban = animelists[animelists['my_watched_episodes'] == 65535]['username'].unique()
+def get_users_for_episode_count_ban(animelists: pd.DataFrame):
+    users_for_ban = pd.Series(animelists[animelists['my_watched_episodes'] == 65535]['username'].unique())
     return users_for_ban
 
 
@@ -22,3 +22,29 @@ def get_users_for_episode_count_ban_cached():
     users_for_ban.to_csv(cache_name, index=False, sep=',', encoding='utf-8')
     return users_for_ban
 
+
+def get_user_days_spent_watching_cached():
+    cache_name = 'cache_user_days_spent_watching.csv'
+    if osp.isfile(cache_name):
+        user_day_spent_watching = pd.read_csv(cache_name, index_col=0, encoding='utf-8')
+        return user_day_spent_watching
+
+    raise Exception('cache not found, must run the basic_cleanup_animelists.ipynb')
+
+
+def get_users_last_list_update_cached():
+    cache_name = 'cache_users_last_list_update.csv'
+    if osp.isfile(cache_name):
+        users_last_list_update = pd.read_csv(cache_name, index_col=0, encoding='utf-8')
+        return users_last_list_update
+
+    raise Exception('cache not found, must run the basic_cleanup_animelists.ipynb')
+
+
+def get_users_stats_episodes_cached():
+    cache_name = 'cache_users_stats_episodes.csv'
+    if osp.isfile(cache_name):
+        users_stats_episodes = pd.read_csv(cache_name, index_col=0, encoding='utf-8')
+        return users_stats_episodes
+
+    raise Exception('cache not found, must run the basic_cleanup_animelists.ipynb')
